2267b6cf2552a7198662e768632b88504ec320e9
messias <messias.inf@gmail.com>
2024-01-31 16:46:42 -0300

    adcionado a tela de router auth

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import https from 'https';
 import http from 'http';
+import siteRoutes from './routes/site';
+import { requestIntercepter } from './util/requestIntercepter';
 
 
 const app = express();
@@ -11,10 +13,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.all('*', requestIntercepter);
+
+//app.use('/admin' adminRoutes);
+app.use('/', siteRoutes);
+
 const runServer = (port: number, server: http.Server) => {
     server.listen(port, () => {
         console.log(`Running at PORT ${port}`);
     });
+}
+
+const regularServer = http.createServer(app);
+if(process.env.NODE_ENV === 'production' ) {
+    // TODO: configurar SSL 
+
+    // TODO: rodar server na 80 na 443
+} else {
+    const serverPort: number = process.env.PORT ? parseInt(process.env.PORT)  :  9000;
+    runServer(serverPort, regularServer);
 }
 
 
